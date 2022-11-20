dca1cf7b8dc9c3d78c542be5afc1fec7af12a695
RifkyRMD27 <99176319+RifkyRMD27@users.noreply.github.com>
2022-11-20 20:52:53 +0700

    Add files via upload
    push

---
+++ TUGAS/Latihan/Pertemuan 7/latihan/latihan 7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      
+               <title>Alret Box</title>
+    </head>
+    <body> 
+        <?php
+        $nilai_1 = 10;
+        $nilai_2 = 10;
+        $nilai_3 = 2 * $nilai_1 + 8 *$nilai_2;        
+        echo "nilai = ", $nilai_3;
+        echo "<br>"
+        $jumlah = $nilai_1 + $nilai_2;
+        echo "hasildari $nilai_1 + nilai_2 adalah: $jumlah"; echo "<br><br>";
+        echo "\" Nama: Agung Teguh Wibowo Almas\" <br>";
+        echo "NIM: 12345678123"
+        ?>
+    </body>
+</html>
